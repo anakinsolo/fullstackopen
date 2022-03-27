96521980f5b3ff78e357deeadab79ec932855d73
Tuan Dinh <darthvader0510@gmail.com>
2022-03-28 02:29:44 +0700

    unicafe exercises

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Label label={'Give Feedbacks'} />
+      <Button label={'Good'} counter={good} setCounter={setGood} /><Button label={'Bad'} counter={bad} setCounter={setBad} /><Button label={'Neutral'} counter={neutral} setCounter={setNeutral} />
+
+      <Label label={'Statistics'} />
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
-  );
+  )
 }
 
-export default App;
+const Label = ({label}) => {
+  return <h1>{label}</h1>
+}
+
+const Button = ({label, counter, setCounter}) => {
+  return (
+      <button onClick={() => setCounter(counter + 1)}>
+        {label}
+      </button>
+  )
+}
+
+const Statistics = ({good, bad, neutral}) => {
+  let total = good + bad + neutral;
+  let avg = (good - bad) / total
+  let positive = good / total * 100
+  if (total === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  } else {
+    return (
+      <table>
+        <StatisticLine text={'Good'} value={good} />
+        <StatisticLine text={'Bad'} value={bad} />
+        <StatisticLine text={'Neutral'} value={neutral} />
+        <StatisticLine text={'Total'} value={total} />
+        <StatisticLine text={'Average'} value={avg} />
+        <StatisticLine text={'Positive'} value={positive} isPositive={true} />
+      </table>
+    )
+  }
+}
+
+const StatisticLine = ({text, value, isPositive}) => {
+  if (isPositive === true) {
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value} %</td>
+      </tr>
+    )
+  } else {
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    )
+  }
+}
+
+export default App
