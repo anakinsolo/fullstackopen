226a1543577c0ba88931eca9b1ebc0195dd99191
Tuan Dinh <darthvader0510@gmail.com>
2022-03-30 21:16:36 +0700

    data for country step 2

--- part2/data-for-countries/src/components/Country.js
+++ part2/data-for-countries/src/components/Country.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import CountryDetail from "./CountryDetail";
 
 const Country = ({country}) => {
+  const [isShow, setIsShow] = useState(false);
+  const showCountryDetail = () => {
+    if (isShow === true) {
+      setIsShow(false);
+    }
+
+    if (isShow === false) {
+      setIsShow(true);
+    }
+  }
+console.log(isShow)
   return (
     <div>
-      {country.name.common}
+      {country.name.common} <button id="show-detail" onClick={showCountryDetail}>show</button>
+      {isShow ? <CountryDetail country={country} /> : ''}
     </div>
   );
 }
